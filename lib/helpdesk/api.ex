f7901afc6e85b15905b75e17517dd9995e6d1343
Moxley Stratton <moxley.stratton@gmail.com>
2023-01-28 21:31:09 -0800

    Fix Helpdesk.Api

--- lib/helpdesk/api.ex
+++ lib/helpdesk/api.ex
@@ -8,4 +8,8 @@ defmodule Helpdesk.Api do
     # Defaults to `true`, use this to disable authorization for the entire API (you probably only want this while prototyping)
     authorize?(false)
   end
+
+  resources do
+    registry Helpdesk.Support.Registry
+  end
 end
